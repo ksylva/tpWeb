8645ae8eb27d7a532686d207b49bed19628b13da
lekaiser <sylvanus010@gmail.com>
2021-03-24 17:12:04 +0100

    Start interaction.js implementation

--- interaction.js
+++ interaction.js
@@ -3,8 +3,12 @@
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
 	// Définir ici les attributs de la 'classe'
+  this.initialPos = 0;
+  this.finalPos = 0;
+  this.currentPos = 0;
 
 	// Developper les 3 fonctions gérant les événements
+
 
 	// Associer les fonctions précédentes aux évènements du canvas.
 };
